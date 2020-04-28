adb9b924e188c44078dd27dfb38fbf190b2e17f2
Andre Oliveira <desmonplays@gmail.com>
2020-04-27 20:57:26 -0400

    (new feature) hello world mapping controller

--- src/main/java/http/github/io/oliveiraandre/sales/SalesApplication.java
+++ src/main/java/http/github/io/oliveiraandre/sales/SalesApplication.java
@@ -2,9 +2,17 @@ package http.github.io.oliveiraandre.sales;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@RestController
 public class SalesApplication {
+
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return "hello world";
+  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesApplication.class, args);
